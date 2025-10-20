7049965c5d292d4743a64ddddee45ac053078aa9
Jonathan <jona.rierasanchez@gmail.com>
2025-10-20 23:00:03 +0200

    small fixes for server initial setup

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
-import express from "express";
+import express, { json } from "express";
 import dotenv from "dotenv";
 
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 4000;
 
 app.use(json());
 
 app.get("/", (req, res) => {
   res.send("App running");
 });
+
 app.listen(PORT, () => {
-  console.log(`App runing on http://localhost:${PORT}!`);
+  console.log(`App running on: http://localhost:${PORT}`);
 });
