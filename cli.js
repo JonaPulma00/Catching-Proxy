183e1a83614c449085a0160ef06675745782aa7b
Jonathan <jona.rierasanchez@gmail.com>
2025-10-20 22:29:39 +0200

    testing commander.js and shebang

--- cli.js
+++ cli.js
@@ -9,12 +9,18 @@ program
   .description("CLI app for testing commander.js")
   .version("1.0.0")
   .option("-d, --debug", "output extra debussing information")
-  .option("-t, --timeout <seconds>", "specify the timeout in seconds", "60");
+  .option("-t, --timeout <seconds>", "specify the timeout in seconds", "60")
+  .command("list")
+  .description("List all items")
+  .action(() => {
+    console.log("Listing items...");
+  });
 
-program.parse();
+program.parse(process.argv);
 
 const options = program.opts();
 
 if (options.debug) {
   console.log("You entered debug mode!");
+  console.log("Options: ", options);
 }
