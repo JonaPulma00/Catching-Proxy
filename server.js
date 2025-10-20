38f555970aa4fd7555c65facda7b6ad6550f0652
Jonathan <jona.rierasanchez@gmail.com>
2025-10-20 13:27:23 +0200

    Basic express server config

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
 import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(json());
+
+app.get("/", (req, res) => {
+  res.send("App running");
+});
+app.listen(PORT, () => {
+  console.log(`App runing on http://localhost:${PORT}!`);
+});
