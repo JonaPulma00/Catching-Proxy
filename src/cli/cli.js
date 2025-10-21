81873ea4f1663d167b95d8bea579f83e8262e673
Jonathan <jona.rierasanchez@gmail.com>
2025-10-22 00:38:00 +0200

    adding basic command options

--- src/cli/cli.js
+++ src/cli/cli.js
@@ -1,20 +1,35 @@
 #!/usr/bin/env node
 
 import { Command } from "commander";
+import chalk from "chalk";
+import chalkAnimation from "chalk-animation";
+import { createSpinner } from "nanospinner";
 
 const program = new Command();
 
-program
-  .name("cli-test")
-  .description("CLI app for testing commander.js")
-  .version("1.0.0")
-  .option("-d, --debug", "output extra debussing information")
-  .option("-t, --timeout <seconds>", "specify the timeout in seconds", "60")
-  .command("list")
-  .description("List all items")
-  .action(() => {
-    console.log("Listing items...");
+const sleep = (ms = 2000) =>
+  new Promise((r) => {
+    setTimeout(r, ms);
   });
+
+program
+  .name("catching-proxy")
+  .description("Catching proxy ")
+  .version("1.0.0")
+  .option(
+    "-p, --port <number>",
+    "specify the port number you want the express server running on (default value is 3000)",
+    "3000"
+  )
+  .option("-o, --origin <url>", "specify the url you want the proxy to catch");
+// .command("list")
+// .description("List all items")
+// .action(async () => {
+//   console.log(chalk.bgGreen("Listing items..."));
+//   createSpinner("wait a second").start();
+//   await sleep();
+//   console.log("Done!");
+// });
 
 program.parse(process.argv);
 
